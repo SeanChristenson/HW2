78d213bb847833a93541d2b4869a75eafcb2f0c9
SeanChristenson <123426816+SeanChristenson@users.noreply.github.com>
2024-01-29 21:23:29 -0700

    rwe
    wre

---
+++ script.js
@@ -0,0 +1,18 @@
+function addParagraph(text) {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = text;
+    document.body.appendChild(paragraph);
+}
+
+addParagraph("My favorite sites!");
+addParagraph("Youtube");
+addParagraph("Facebook");
+addParagraph("Yahoo");
+addParagraph("My favorite games!");
+addParagraph("Last Oasis");
+addParagraph("League of Legends");
+addParagraph("Rust");
+addParagraph("My favorite artists and why they are important!");
+addParagraph("Elvis Presley, essentially was the godfather of rock n roll.");
+addParagraph("Michelangelo, was a main factor in the italian renaissance such as the statue of david.");
+addParagraph("Michael Jackson, revolutionized pop in a new direction and broke racial boundaries.");
